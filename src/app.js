51021cd1ff0574ce71194312003b5ddcddea57fa
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-10-21 15:24:34 +0100

    Remove npm workspaces
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import { compose } from "./middlewares/mod.js";
+
+/** @typedef {import("node:http").IncomingMessage} Request */
+/** @typedef {import("node:http").ServerResponse} Response */
+/** @typedef {(req: Request, req: Response, next: () => Promise<void> | void) => Promise<void> | void} Middleware */
+
+class App {
+	/** @type {Middleware[]} */
+	#middlewares = [];
+
+	/** @type {Middleware} */
+	#middleware;
+
+	use(/** @type {Middleware[]} */ ...m) {
+		this.#middlewares.push(...m.filter((f) => typeof f === "function"));
+		this.#middleware = compose(this.#middlewares);
+	}
+
+	handle() {
+		return this.#middleware;
+	}
+}
+
+export default App;
---
+++ src/middlewares/compose-middleware.js
@@ -0,0 +1,27 @@
+/** @typedef {import("node:http").IncomingMessage} Request */
+/** @typedef {import("node:http").ServerResponse} Response */
+/** @typedef {(req: Request, req: Response, next: () => Promise<void> | void) => Promise<void> | void} Middleware */
+
+/**
+ * @param {Middleware[]} fns
+ * @returns {Middleware}
+ */
+const composeMiddleware = (fns) => {
+	return function handler(request, response) {
+		let i = 0;
+
+		return step();
+
+		function step() {
+			const m = fns[i];
+
+			if (!m) return;
+
+			i += 1;
+
+			return m(request, response, step);
+		}
+	};
+};
+
+export default composeMiddleware;
