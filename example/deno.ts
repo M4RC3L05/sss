859ed7afaebdb43a11b8165191d0191d60c212d8
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-10-28 15:59:43 +0100

    Add exmaples
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ example/deno.ts
@@ -0,0 +1,30 @@
+// @ts-nocheck
+
+import { App, DenoRuntime, Router } from "../src/mod.js";
+
+// Optionally pass the correct js runtime.
+const app = new App<DenoRuntime>();
+// You can pass `find-my-way` options to the first argument of setup.
+const router = await new Router<DenoRuntime>().setup();
+
+router.get("/foo", (request) =>
+  Response.json({
+    ok: true,
+    params: request.params,
+    searchParams: request.searchParams
+  })
+);
+
+// If your middlewares are async and want to do something after the response them,
+// you must await the next, otherwise just call next.
+app.use((request, next) => {
+  console.log("before");
+  const response = next();
+  console.log("after");
+
+  return response;
+});
+app.use(router.middleware());
+app.use((request) => Response.json({ message: "Not found" }, { status: 404 }));
+
+Deno.serve({ port: 4321 }, app.handle())
---
+++ example/node.ts
@@ -0,0 +1,39 @@
+// @ts-nocheck
+
+import { createServer } from "node:http";
+import { App, NodeRuntime, Router } from "../src/mod.js";
+
+// Optionally pass the correct js runtime.
+const app = new App<NodeRuntime>();
+// You can pass `find-my-way` options to the first argument of setup.
+const router = await new Router<NodeRuntime>().setup();
+
+router.get("/foo", (request, response) => {
+  response.statusCode = 200;
+
+  response.setHeader("content-type", "application/json");
+  response.end(JSON.stringify({
+    ok: true,
+    params: request.params,
+    searchParams: request.searchParams
+  }));
+})
+
+// If your middlewares are async and want to do something after the response them,
+// you must await the next, otherwise just call next.
+app.use((request, response, next) => {
+  console.log("before");
+  next();
+  console.log("after");
+});
+app.use(router.middleware());
+app.use((request, response) => {
+  response.statusCode = 404;
+
+  response.setHeader("content-type", "application/json");
+  response.end(JSON.stringify({ message: "Not found" }));
+})
+
+const server = createServer(app.handle());
+
+server.listen(4321);
